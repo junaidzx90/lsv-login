ba4c69a691bd269a448b01c42d735d296e2fdc23
junaidzx90 <jsjunu@gmail.com>
2021-05-20 12:32:05 +0600

    first commit

---
+++ includes/class-lsv-login-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://example.com
+ * @since      1.0.0
+ *
+ * @package    Lsv_Login
+ * @subpackage Lsv_Login/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Lsv_Login
+ * @subpackage Lsv_Login/includes
+ * @author     Benakhigbe <benakhigbe.contact@gmail.com>
+ */
+class Lsv_Login_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
